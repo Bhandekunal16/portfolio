e151c5e24925a922181c6f93426fda76b869dd03
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-22 17:58:55 +0530

    "Refactored About component to use ngFor directive and moved hardcoded lists to component variables"

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-
+  skills: string[] = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'SCSS',
+    'Python',
+    'C',
+    'C++',
+    'C#',
+  ];
+  backendSkills: string[] = [
+    'Node.js',
+    'Nest.js',
+    'Angular.js',
+    'React.js',
+    'Flask',
+    'React Native',
+    '.net',
+  ];
+  roboticSkills: string[] = ['robotic.js', 'roboticdb', 'authanticator'];
 }
