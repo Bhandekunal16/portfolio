9bf3360960705b0b9a55b92751a7cba79a38cb60
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-22 11:59:37 +0530

    "Added contact form component with HTML, SCSS, and TypeScript code"

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError, throwError } from 'rxjs';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  public myForm: FormGroup;
 
+  constructor(private http: HttpClient) {
+    this.myForm = new FormGroup({
+      email: new FormControl(''),
+      message: new FormControl(''),
+    });
+  }
+
+  ngOnInit(): void {}
+
+  submit() {
+    const email = this.myForm.value.email;
+    const message = this.myForm.value.message;
+
+    this.email({
+      to: email,
+      message: `your message received successfully.`,
+    }).subscribe((ele) => {
+      console.log(ele);
+    });
+    this.email({
+      to: 'roboticdev07@gmail.com',
+      message: message,
+    }).subscribe((ele) => {
+      console.log(ele);
+    });
+  }
+
+  email(body: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>(
+        'https://mailer-service-eight.vercel.app/message/send-email',
+        body,
+        { headers }
+      )
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
 }
