25b5f8b5e0b0eb47427e9c4397421e1a068640bf
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-12 16:24:46 +0530

    "Added HttpClientModule to StatusComponent imports, injected HttpClient into constructor, and created register method to make HTTP POST request"

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -9,11 +9,23 @@ import {
 import { ButtonModule } from 'primeng/button';
 import { MessagesModule } from 'primeng/messages';
 import { Message } from 'primeng/api';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-status',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, MessagesModule, ButtonModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MessagesModule,
+    ButtonModule,
+    HttpClientModule,
+  ],
   templateUrl: './status.component.html',
   styleUrl: './status.component.scss',
 })
@@ -22,7 +34,7 @@ export class StatusComponent {
   public msg: Message[] | any;
   public options: string[] | any = ['AT HOME', 'AT OFFICE', 'AT TRAVEL'];
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.myForm = new FormGroup({
       userType: new FormControl('', Validators.required),
     });
@@ -33,5 +45,19 @@ export class StatusComponent {
     setInterval(() => {
       window.location.reload();
     }, 3000);
+  }
+
+  register(body: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>('http://localhost:3003/auth/register', body, { headers })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
   }
 }
