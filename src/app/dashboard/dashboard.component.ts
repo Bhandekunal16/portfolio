6eddd538bad86bdeb988c85a135300367584b4d6
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-12 17:05:32 +0530

    "Moved assignment of 'this.status' and rearranged 'this.item' array initialization"

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -44,41 +44,43 @@ export class DashboardComponent {
         key: 'robotic.js',
         data: encryptedData,
       }).subscribe((ele) => {
-        this.item = [
-          {
-            label: 'Home',
-            icon: 'pi pi-home',
-            command: () => {
-              this.initial();
-            },
-          },
-          {
-            label: 'Project',
-            icon: 'pi pi-briefcase',
-            command: () => {
-              this.service();
-            },
-          },
-          {
-            label: 'About',
-            icon: 'pi pi-user',
-            command: () => {
-              this.About();
-            },
-          },
-          {
-            label: 'Contact',
-            icon: 'pi pi-envelope',
-            command: () => {
-              this.contact();
-            },
-          },
-          {
-            label: ele.data[0].status,
-            icon: 'pi pi-envelope',
-          },
-        ];
+        this.status = ele.data[0].status
       });
+
+      this.item = [
+        {
+          label: 'Home',
+          icon: 'pi pi-home',
+          command: () => {
+            this.initial();
+          },
+        },
+        {
+          label: 'Project',
+          icon: 'pi pi-briefcase',
+          command: () => {
+            this.service();
+          },
+        },
+        {
+          label: 'About',
+          icon: 'pi pi-user',
+          command: () => {
+            this.About();
+          },
+        },
+        {
+          label: 'Contact',
+          icon: 'pi pi-envelope',
+          command: () => {
+            this.contact();
+          },
+        },
+        {
+          label: this.status,
+          icon: 'pi pi-envelope',
+        },
+      ];
     });
   }
 
