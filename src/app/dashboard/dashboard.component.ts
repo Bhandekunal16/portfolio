d3cbb12994dd6f7852ce26adfb2e69fc0010a086
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-12 16:50:49 +0530

    "Added HttpClient and HttpClientModule imports, injected HttpClient into DashboardComponent, and implemented list and decrypt methods to make API calls."

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,17 +2,24 @@ import { Component } from '@angular/core';
 import { MenubarModule } from 'primeng/menubar';
 import { ButtonModule } from 'primeng/button';
 import { Router } from '@angular/router';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MenubarModule, ButtonModule],
+  imports: [MenubarModule, ButtonModule, HttpClientModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
   public item: any[] | undefined;
+  public status: any = '';
 
   About(): void {
     this.router.navigate(['/about']);
@@ -31,6 +38,19 @@ export class DashboardComponent {
   }
 
   ngOnInit() {
+    this.list().subscribe((ele) => {
+      const encryptedData = ele.encrypted;
+
+      this.decrypt({
+        key: 'robotic.js',
+        data: encryptedData,
+      }).subscribe((ele) => {
+        console.log(ele);
+
+        this.status = ele.data[0].status;
+      });
+    });
+
     this.item = [
       {
         label: 'Home',
@@ -61,9 +81,41 @@ export class DashboardComponent {
         },
       },
       {
-        label: localStorage.getItem('status'),
+        label: this.status,
         icon: 'pi pi-envelope',
-      }
+      },
     ];
+  }
+
+  list(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .get<any>(`https://device-probe.vercel.app/get/portfolio/status`, {
+        headers,
+      })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
+
+  decrypt(body: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>('https://device-probe.vercel.app/decrypt', body, {
+        headers,
+      })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
   }
 }
