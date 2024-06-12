9bf8e1e7a3c806800ffb2425d7e91a3f8352cbb1
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-12 10:00:51 +0530

    "Added forget password form and styling to status component"

--- src/app/status/status.component.ts
+++ src/app/status/status.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { MessagesModule } from 'primeng/messages';
+import { Message } from 'primeng/api';
 
 @Component({
   selector: 'app-status',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, MessagesModule, ButtonModule],
   templateUrl: './status.component.html',
-  styleUrl: './status.component.scss'
+  styleUrl: './status.component.scss',
 })
 export class StatusComponent {
+  public myForm: FormGroup;
+  public msg: Message[] | any;
 
+  constructor() {
+    this.myForm = new FormGroup({
+      email: new FormControl(''),
+    });
+  }
+  submitForm() {}
 }
