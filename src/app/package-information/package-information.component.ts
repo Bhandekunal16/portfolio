c0a814a1c789bdff19bcb874ddda84ac849042b3
bhandekunal16 <bhandekunal16@gmail.com>
2024-07-03 12:54:32 +0530

    "Added new HTML template code to display inner items of dataList and modified the component.ts file to populate dataList with dependencies from API response."

--- src/app/package-information/package-information.component.ts
+++ src/app/package-information/package-information.component.ts
@@ -16,6 +16,8 @@ import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './package-information.component.scss',
 })
 export class PackageInformationComponent {
+
+dataList: any;
   constructor(private http: HttpClient) {
     this.myForm = new FormGroup({
       PackageName: new FormControl(''),
@@ -35,37 +37,45 @@ export class PackageInformationComponent {
   public myForm: FormGroup;
   public nodes!: any[];
   public selectedNodes: any;
+  public data2: any[] = [];
 
-  call(){
-    this.info({name : this.myForm.get('PackageName')?.value}).subscribe((ele) => {
-      this.name = ele.data.name?.toUpperCase();
-      this.createdOn = new Date(ele.data.time.created).toDateString();
-      this.modifiedOn = new Date(ele.data.time.modified).toDateString();
-      this.author = ele.data.author.name;
-      this.description = ele.data.description;
-      this.maintainers = ele.data.maintainers[0].name;
-      this.license = ele.data.license;
+  call() {
+    this.info({ name: this.myForm.get('PackageName')?.value }).subscribe(
+      (ele) => {
+        this.name = ele.data.name?.toUpperCase();
+        this.createdOn = new Date(ele.data.time.created).toDateString();
+        this.modifiedOn = new Date(ele.data.time.modified).toDateString();
+        this.author = ele.data.author.name;
+        this.description = ele.data.description;
+        this.maintainers = ele.data.maintainers[0].name;
+        this.license = ele.data.license;
 
-      const obj = ele.data.time;
-      const arr = [];
-      for (let key in obj) {
-        arr.push({ key: key, value: obj[key] });
+        const obj = ele.data.time;
+        const arr = [];
+        for (let key in obj) {
+          arr.push({ key: key, value: obj[key] });
+        }
+        this.date = arr;
+
+        const obj2 = ele.data.versions;
+        const arr2: any[] = [];
+        for (let key in obj2) {
+          arr2.push({ key: key, value: obj2[key] });
+        }
+        this.data = arr2;
+
+        const next = [];
+  
+
+        for (let index = 0; index < this.data.length; index++) {
+          const dependencies = this.data[index].value.dependencies;
+          next.push([dependencies]);
+        }
+
+        this.dataList = next
+        console.log(next);
       }
-      this.date = arr;
-
-
-      const obj2 = ele.data.versions;
-      console.log(obj2)
-      const arr2: any[] = [];
-      for (let key in obj2) {
-        arr2.push({ key: key, value: obj2[key] });
-      }
-      this.data = arr2;
-      console.log(this.data)
-    });
-
-
-
+    );
   }
 
   formatDate(date: any): string {
